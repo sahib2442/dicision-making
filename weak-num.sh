3d59996696d06490ee9e64bfa05ded067049499d
sahib2442 <kadersahib1234@gmail.com>
2022-08-04 09:51:47 +0530

    week-num

---
+++ weak-num.sh
@@ -0,0 +1,19 @@
+read -p "enter the week number" week-num
+echo "week number" $week-num
+case $week-num in
+1)echo "sunday"
+     ;;
+2)echo "monday"
+     ;;
+3)echo "tuesday"
+     ;;
+4)echo "wednesday"
+     ;;
+5)echo "thursday"
+     ;;
+6)echo "friday"
+     ;;
+7)echo "saturday"
+     ;;
+*)echo "invalied week"
+esac
